8c67cb8f9554d05841e0733d9d217831c9179692
Arielly <maria.lima@sou.inteli.edu.br>
2026-04-08 22:59:07 -0300

    feat: adiciona codigo arduino

--- arduino/estacao.ino
+++ arduino/estacao.ino
@@ -1,22 +1,29 @@
 #include "DHT.h"
+
 #define DHTPIN 2
 #define DHTTYPE DHT11
+
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-  Serial.begin(9600); // Frequência de comunicação [cite: 85]
-  dht.begin();
+  Serial.begin(9600); // [cite: 85]
+  dht.begin();        // [cite: 86]
 }
 
 void loop() {
-  float temp = dht.readTemperature();
+  float temp = dht.readTemperature(); // [cite: 89]
   float umid = dht.readHumidity();
 
-  if (!isnan(temp) && !isnan(umid)) {
-    // Formato JSON para o script Python ler facilmente [cite: 78, 92]
-    Serial.print("{\"temperatura\":"); Serial.print(temp);
-    Serial.print(",\"umidade\":"); Serial.print(umid);
-    Serial.println("}");
+  // Simulação caso os sensores físicos não estejam disponíveis [cite: 98]
+  if (isnan(temp) || isnan(umid)) {
+    temp = random(200, 320) / 10.0; // Valores aleatórios realistas [cite: 98]
+    umid = random(400, 700) / 10.0;
   }
-  delay(5000); // Leitura a cada 5 segundos [cite: 78, 96]
+
+  // Envio formatado em JSON para leitura pelo script Python [cite: 78, 92, 95]
+  Serial.print("{\"temperatura\":"); Serial.print(temp);
+  Serial.print(",\"umidade\":"); Serial.print(umid);
+  Serial.println("}");
+
+  delay(5000); // Intervalo de 5 segundos [cite: 96]
 }
